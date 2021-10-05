425e863213d979c3ba46693a75ed2daa15f52da3
Pavel-Tsarev <tsarev.pn@phystech.edu>
2021-10-05 13:52:50 +0300

    1 part of changes

--- 28-09/3.py
+++ 28-09/3.py
@@ -5,54 +5,84 @@ import random
 pygame.init()
 
 def tree(x, y, w):
-    pygame.draw.polygon(screen, (190, 190, 0), ((x, 0), (x+w, 0), (x+w, y), (x, y)))
+    '''
+    Function draws tree from the top of the screen.
+    x - coordinate of the left edge
+    y - coordinate of the low edge
+    w - width of tree trunk
+    '''
+    polygon(screen, (190, 190, 0), ((x, 0), (x + w, 0), (x + w, y), (x, y)))
 
 def koluchka(x, y, s):
+    '''
+    Function draws hedgehog's spike.
+    x, y - coordinates of the low left vertex of the spike
+    s - coefficient of compression/stretching
+    s > 0
+    '''
     w = 5*s
     l = 50*s
     a = random.randint(-30, 30)
     a = a/180*np.pi
-    pygame.draw.polygon(screen, (0, 0, 0), ((x, y), (x+w*np.cos(a), y + w*np.sin(a)),
+    polygon(screen, (0, 0, 0), ((x, y), (x + w*np.cos(a), y + w*np.sin(a)),
                                 (x + w/2*np.cos(a) + l*np.sin(a), y + w/2*np.sin(a) - l*np.cos(a))))
+
 def iozh(x, y, s):
+    '''
+    Function draws hedgehog.
+    x, y - coordinates of the left hedgehog's edge
+    s - coefficient of compression/stretching
+    s > 0
+    '''
     ellipse(screen, (60, 60, 60), (x, y, 100*s, 70*s))
-    ellipse(screen, (60, 60, 60), (x+80*s, y+55*s, 20 * s, 10 * s))
-    ellipse(screen, (60, 60, 60), (x, y+55*s, 20 * s, 10* s))
-    ellipse(screen, (60, 60, 60), (x - 10*s, y + 40 * s, 20 * s, 10 * s))
-    ellipse(screen, (60, 60, 60), (x + 92*s, y + 45 * s, 10 * s, 10 * s))
-    ellipse(screen, (60, 60, 60), (x + 80*s, y + 25 * s, 40 * s, 20 * s))
-    circle(screen, (0, 0, 0), (x + 120*s, y + (35) * s), 2 * s)
-    circle(screen, (0, 0, 0), (x + 110 * s, y + (30) * s), 3 * s)
-    circle(screen, (0, 0, 0), (x + 105 * s, y + (35) * s), 3 * s)
+    ellipse(screen, (60, 60, 60), (x + 80*s, y + 55*s, 20*s, 10*s))
+    ellipse(screen, (60, 60, 60), (x, y + 55*s, 20*s, 10*s))
+    ellipse(screen, (60, 60, 60), (x - 10*s, y + 40*s, 20*s, 10*s))
+    ellipse(screen, (60, 60, 60), (x + 92*s, y + 45*s, 10*s, 10*s))
+    ellipse(screen, (60, 60, 60), (x + 80*s, y + 25*s, 40*s, 20*s))
+    circle(screen, (0, 0, 0), (x + 120*s, y + 35*s), 2*s)
+    circle(screen, (0, 0, 0), (x + 110*s, y + 30*s), 3*s)
+    circle(screen, (0, 0, 0), (x + 105*s, y + 35*s), 3*s)
     for i in range(10*s, 95*s, 10*s):
         for k in range(30*s, 65*s, 10*s):
             if not ((i > 70*s) and (k > 55*s)):
-                koluchka(x+i, y+k, s)
+                koluchka(x + i, y + k, s)
     apple(x, y, s)
-    apple(x +10*s, y, s)
-    grib(x + 50*s, y, s)
+    apple(x + 10*s, y, s)
+    grib (x + 50*s, y, s)
     for i in range(10*s, 95*s, 10*s):
         for k in range(30*s, 65*s, 10*s):
             if not ((i > 79*s) and (k > 55*s)):
-                koluchka(x+i, y+k, s)
+                koluchka(x + i, y + k, s)
+
 def grib(x, y, s):
+    '''
+    Function draws mushroom.
+    x, y - coordinates of the left border of the mushroom's leg
+    s - coefficient of compression/stretching
+    s > 0
+    '''
     ellipse(screen, (255, 255, 255), (x, y , 30*s , 50*s))
-    ellipse(screen, (255, 0, 0), (x-20*s, y, 70 * s, 20*s))
-    ellipse(screen, (255, 255, 255), (x-5*s, y+5*s, 10*s, 3*s))
-    ellipse(screen, (255, 255, 255), (x+10*s, y+5*s, 4*s, 3* s))
-    ellipse(screen, (255, 255, 255), (x + 25 * s, y + 5 * s, 10 * s, 3 * s))
-    ellipse(screen, (255, 255, 255), (x + 25 * s, y + 10 * s, 7 * s, 3 * s))
-    ellipse(screen, (255, 255, 255), (x + 10 * s, y + 10 * s, 7 * s, 3 * s))
+    ellipse(screen, (255, 0, 0), (x - 20*s, y, 70*s, 20*s))
+    ellipse(screen, (255, 255, 255), (x - 5*s, y + 5*s, 10*s, 3*s))
+    ellipse(screen, (255, 255, 255), (x + 10*s, y + 5*s, 4*s, 3* s))
+    ellipse(screen, (255, 255, 255), (x + 25*s, y + 5*s, 10*s, 3*s))
+    ellipse(screen, (255, 255, 255), (x + 25*s, y + 10*s, 7*s, 3*s))
+    ellipse(screen, (255, 255, 255), (x + 10*s, y + 10*s, 7*s, 3*s))
 
 def apple(x, y, s):
+    '''
+    Function draws apple.
+    x, y - coordinates of the left edge of apple
+    s - coefficient of compression/stretching
+    s > 0
+    '''
     ellipse(screen, (255, 0, 0), (x, y , 30*s , 30*s))
-
-
 
 FPS = 30
 screen = pygame.display.set_mode((800, 1000))
 screen.fill((0, 255, 0))
-pygame.draw.polygon(screen, (100, 100, 100), ((0, 600), (0, 1000), (800, 1000), (800, 600)))
+polygon(screen, (100, 100, 100), ((0, 600), (0, 1000), (800, 1000), (800, 600)))
 tree(0, 650, 50)
 iozh(200, 600, 1)
 tree(100, 980, 150)
@@ -76,4 +106,5 @@ while not finished:
             finished = True
 
 pygame.quit()
+
 
